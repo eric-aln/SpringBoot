a9cab2cc284ad28a74fedb0f5c92d7f473236ebf
eric-aln <102973034+eric-aln@users.noreply.github.com>
2022-06-05 18:41:22 -0300

    v2.0

---
+++ blogpessoal/repository/TemaRepository.java
@@ -0,0 +1,12 @@
+package com.example.com.generation.blogpessoal.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.com.generation.blogpessoal.model.Tema;
+
+public interface TemaRepository extends JpaRepository<Tema, Long>{
+	public List<Tema> findAllByDescricaoContainingIgnoreCase(String descricao);
+
+}
